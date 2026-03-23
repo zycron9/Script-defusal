73755374466c66da53a20c0ab37ec351b1b08d89
zycron9 <cleuzasilva7010@gmail.com>
2026-03-22 22:21:48 -0300

    Remove Adonis bypass logic from PasteWare_Rayfield.lua
    Removed bypass logic for Adonis based on user input services.

--- PasteWare_Rayfield.lua
+++ PasteWare_Rayfield.lua
@@ -1,9 +1,3 @@
-local UIS = game:GetService("UserInputService")
-if UIS.TouchEnabled and not UIS.MouseEnabled and not UIS.KeyboardEnabled then
-    getgenv().bypass_adonis = true
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/FakeAngles/PasteWare-v2/refs/heads/main/PasteWare.lua'))() return
-end
-
 if not game:IsLoaded() then
     game.Loaded:Wait()
 end
